285629aeaccb23bbac9d1ac0ead84b115889a192
Kainã Terto <kterto@capyba.com>
2025-07-14 09:19:06 -0300

    connected and working

---
+++ README.md
@@ -0,0 +1,14 @@
+# FIREBASE MESSAGING LAB
+
+This is just a simple app to connect and send messages using Firebase Cloud Messaging (FCM).
+
+## .env
+
+follow the `.env.example` file, and place your service account file in the projects root directory. You can edit the test message on `index.js` using `message` object. To send test messages use:
+
+`npm run send-message`
+
+## relevant documenation
+
+- [https://firebase.google.com/docs/cloud-messaging/send-message#node.js][url]
+- [https://firebase.google.com/docs/cloud-messaging/auth-server][url]
---
+++ index.js
@@ -0,0 +1,48 @@
+var admin = require("firebase-admin");
+var getMessaging = require("firebase-admin/messaging").getMessaging;
+var serviceAccount = require(`${process.env.SERVICE_ACCOUNT}`);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+// device token
+// This registration token comes from the client FCM SDKs.
+// You can get it from the client app when the user registers for notifications.
+const registrationToken =
+  "ffeD-w8iQseHoksBsjmGQD:APA91bGBXa7ghjrbxuAjDQdA-qiogElPS3U-7re3ZuOzPXSVQ93-CcaVOj8TD9t1RSlC7bER3iKmwZgelNY5vv8xmyq6-d56Tedq7vszfpgzVVbWPkv5EFg";
+
+const message = {
+  token: registrationToken,
+  notification: {
+    title: "Test Notification",
+    body: "This is a test with an image",
+  },
+  android: {
+    notification: {
+      imageUrl: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
+    },
+  },
+  apns: {
+    payload: {
+      aps: {
+        mutableContent: true, // This allows the notification to be modified by app extensions
+      },
+    },
+  },
+  data: {
+    imageUrl: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
+  },
+};
+
+// Send a message to the device corresponding to the provided
+// registration token.
+getMessaging()
+  .send(message)
+  .then((response) => {
+    // Response is a message ID string.
+    console.log("Successfully sent message:", response);
+  })
+  .catch((error) => {
+    console.log("Error sending message:", error);
+  });
